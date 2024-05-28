f382639ebec56e52397e2e0bb7f2ded783ecce59
likezheng <nicol@fuseinsurtech.com>
2024-05-29 00:55:09 +0800

    CS-4197: see

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.get("/test.png",(req,res)=>{
     res.sendFile(__dirname+"/test.png")        
 })
+app.get("/adapter-latest.js",(req,res)=>{
+    res.sendFile(__dirname+"/adapter-latest.js")        
+})
 
 app.get("/index.html",(req,res)=>{
     res.sendFile(__dirname+"/"+"index.html")    
